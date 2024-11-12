155d1e5792df6864d232150cd3e578e2ab86b329
Nils Holzer <kroko.holzer@gmail.com>
2024-11-12 18:02:24 +0100

    Added task b) of the first assignment

--- p1/pointers.c
+++ p1/pointers.c
@@ -18,7 +18,16 @@ int average(int *arrayPointer, unsigned int size)
 
 int averageIndirect(int **arrayPointer, unsigned int size)
 {
-    (void) arrayPointer;
-    (void) size;
-    return 0;
+    if (size == 0)
+    {
+        return 0;
+    }
+    unsigned int count;
+    int sum = 0;
+    for (count = 0; count < size; count++)
+    {
+        sum += *arrayPointer[count];
+    }
+    
+    return sum / size;
 }
