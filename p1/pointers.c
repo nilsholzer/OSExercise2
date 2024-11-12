412599ed1e60478a97e737c29936486e351379ab
Nils Holzer <kroko.holzer@gmail.com>
2024-11-12 17:59:25 +0100

    Added task a) of the first assignment and the gitignore

--- p1/pointers.c
+++ p1/pointers.c
@@ -2,9 +2,18 @@
 
 int average(int *arrayPointer, unsigned int size)
 {
-    (void) arrayPointer;
-    (void) size;
-    return 0;
+    if (size == 0)
+    {
+        return 0;
+    }
+    unsigned int count;
+    int sum = 0;
+    for (count = 0; count < size; count++)
+    {
+        sum += arrayPointer[count];
+    }
+    
+    return sum / size;
 }
 
 int averageIndirect(int **arrayPointer, unsigned int size)
